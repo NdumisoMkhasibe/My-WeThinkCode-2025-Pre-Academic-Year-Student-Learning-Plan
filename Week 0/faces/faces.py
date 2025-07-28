e9ac06cc6eebb22fdc532f6770352ebcabfee392
NdumisoMkhasibe <mkhasibendumiso3@gmail.com>
2025-07-28 20:11:15 +0200

    Update faces.py

--- Week 0/faces/faces.py
+++ Week 0/faces/faces.py
@@ -1,1 +1,11 @@
+def main():
+    text = input("Enter text with emoticons: ")
+    text = convert(text)
+    print(text)
 
+def convert(message):
+    message = message.replace(":)","🙂")
+    message = message.replace(":(","🙁")
+    return message
+
+main()
