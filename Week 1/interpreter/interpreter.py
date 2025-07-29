77b59cb9feaf3ecbaf5f56865d8b64e0c47c8b1e
NdumisoMkhasibe <mkhasibendumiso3@gmail.com>
2025-07-29 20:00:08 +0200

    Update interpreter.py

--- Week 1/interpreter/interpreter.py
+++ Week 1/interpreter/interpreter.py
@@ -1,1 +1,13 @@
+xyz = input("Expression: ").strip()
 
+x = float(xyz[0])
+z = float(xyz[4])
+
+if xyz[2] == "+" :
+    print(x+z)
+elif xyz[2] == "-" :
+    print(x-z)
+elif xyz[2] == "*" :
+    print(x*z)
+elif xyz[2] == "/" :
+    print(x/z)
