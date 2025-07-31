48929891549c2bfc410258ba11029b3e7ddb7de7
NdumisoMkhasibe <mkhasibendumiso3@gmail.com>
2025-07-31 22:00:53 +0200

    Update coke.py

--- Week 2/coke/coke.py
+++ Week 2/coke/coke.py
@@ -1,1 +1,11 @@
-
+def main():
+    total = 0
+    while total <50:
+        bal = 50 - total
+        print(f"Amount Due: {bal}")
+        coin = int(input("Insert Coin: "))
+        if coin == 25 or coin == 10 or coin == 5 :
+            total += coin
+    if total >= 50:
+        print(f"Change Owed: {(total-50)}")
+main()
