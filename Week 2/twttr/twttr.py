03b3f7120eefb7a3a4bc2e1de4daf7818cf62384
NdumisoMkhasibe <mkhasibendumiso3@gmail.com>
2025-07-31 22:02:45 +0200

    Update twttr.py

--- Week 2/twttr/twttr.py
+++ Week 2/twttr/twttr.py
@@ -1,1 +1,17 @@
 
+def main():
+    text = input("Input: ").strip()
+    final = omitter(text)
+    print("Output: "+ final)
+
+def omitter(msg):
+    new_msg = ""
+    for char in msg:
+        if char in "aeiouAEIOU":
+            pass
+        else:
+            new_msg = new_msg + char
+    return new_msg
+
+if __name__ == "__main__":
+    main()
