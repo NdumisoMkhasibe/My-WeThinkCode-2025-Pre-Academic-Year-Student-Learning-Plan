84ac783d8d5d7065261e04ea25da63ecc4ce7538
NdumisoMkhasibe <mkhasibendumiso3@gmail.com>
2025-07-31 21:59:18 +0200

    Update camel.py

--- Week 2/camel/camel.py
+++ Week 2/camel/camel.py
@@ -1,1 +1,18 @@
+def main():
+    camel = input("camelCase: ").strip()
+    output = snake(camel)
+    print("snake_case:"+ output)
 
+def snake(text):
+    new_text = ""
+    for char in text:
+        if char.isupper():
+            char = char.lower()
+            new_text = new_text + "_" + char
+        else:
+            char = char.lower()
+            new_text = new_text + char
+
+    return new_text
+
+main()
