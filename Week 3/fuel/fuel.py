958035e8a144bee7685922b7d5828a284379dec9
NdumisoMkhasibe <mkhasibendumiso3@gmail.com>
2025-08-05 19:58:09 +0200

    Update fuel.py

--- Week 3/fuel/fuel.py
+++ Week 3/fuel/fuel.py
@@ -1,1 +1,30 @@
+def main():
+    answer = decifer()
+    if answer <= 1 :
+        print("E")
+    elif answer >= 99 :
+        print("F")
+    else:
+        print(f"{answer}%")
 
+def decifer():
+    while True :
+        try :
+            xy = input("Fraction: ").strip()
+            x,y = xy.split("/")
+            x = int(x)
+            y = int(y)
+            if x == 0:
+                return 0
+            elif x > y :
+                pass
+            else:
+                ans = (x/y)*100
+                ans = round(ans)
+                return ans
+        except ValueError:
+            pass
+        except ZeroDivisionError :
+            pass
+
+main()
